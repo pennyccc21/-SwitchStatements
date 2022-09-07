21d34a269c092f821665086df70fc817d25a9f85
Peter luque <Peterluque21@gmail.com>
2022-09-07 19:27:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); //evaluate too a string switch (subject)
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is an intresting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"oh wow! i havent taken that subject before. {subject} sounds fun !");
+                    break;
+            }
+        }
+    }
+}
